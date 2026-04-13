5d8c0f692ad299b1a72ff20a1ccc428f3bbda361
Michael <mvebicanow.saz@gmail.com>
2026-04-13 10:30:36 +0300

    Edit Sequence.h

--- Sequence.h
+++ Sequence.h
@@ -5,7 +5,7 @@
 #include "MyUtility.h"
 #include "Option.h"
 
-template <class T> class Sequence;
+// Предварительные объявления классов-наследников для методов EnsureArray/EnsureList
 template <class T> class BaseArraySequence;
 template <class T> class BaseListSequence;
 template <class T> class MutableArraySequence;
@@ -28,44 +28,12 @@ public:
 };
 
 template <class T>
-class SequenceEnumerator : public IEnumerator<T> {
-private:
-    const Sequence<T>* sequence;
-    int currentIndex;
-    int length;
-
-public:
-    SequenceEnumerator(const Sequence<T>* seq, int len) 
-        : sequence(seq), currentIndex(-1), length(len) {}
-
-    bool MoveNext() override {
-        currentIndex++;
-        return currentIndex < length;
-    }
-
-    T GetCurrent() const override {
-        if (currentIndex < 0 || currentIndex >= length) {
-            throw IndexOutOfRange(); 
-        }
-        return sequence->Get(currentIndex);
-    }
-
-    void Reset() override {
-        currentIndex = -1;
-    }
-
-    ~SequenceEnumerator() override {}
-};
-
-template <class T>
 class Sequence : public IEnumerable<T> {
 public:
-
     using IEnumerator = ::IEnumerator<T>;
     using IEnumerable = ::IEnumerable<T>;
-    using SequenceEnumerator = ::SequenceEnumerator<T>;
 
-    // Основной интерфейс
+    // Основной интерфейс последовательности
     virtual T Get(int index) const = 0;
     virtual int GetLength() const = 0;
     virtual T GetFirst() const = 0;
@@ -87,22 +55,52 @@ public:
     virtual Sequence<T>* Clone() const = 0;
     virtual Sequence<T>* CreateEmpty() const = 0;
 
+protected:
+    class SequenceEnumeratorImpl : public ::IEnumerator<T> {
+    private:
+        const Sequence<T>* sequence;
+        int currentIndex;
+        int length;
+
+    public:
+        SequenceEnumeratorImpl(const Sequence<T>* seq, int len) 
+            : sequence(seq), currentIndex(-1), length(len) {}
+
+        bool MoveNext() override {
+            currentIndex++;
+            return currentIndex < length;
+        }
+
+        T GetCurrent() const override {
+            if (currentIndex < 0 || currentIndex >= length) {
+                throw IndexOutOfRange(); 
+            }
+            return sequence->Get(currentIndex);
+        }
+
+        void Reset() override {
+            currentIndex = -1;
+        }
+
+        ~SequenceEnumeratorImpl() override {}
+    };
+
+public:
+    // Алиас для вложенного класса реализации.
+    // Позволяет использовать конструкцию Sequence<T>::SequenceEnumerator во внешнем коде.
+    using SequenceEnumerator = SequenceEnumeratorImpl;
     IEnumerator* GetEnumerator() override {
-        return new SequenceEnumerator(this, this->GetLength());
+        return new SequenceEnumeratorImpl(this, this->GetLength());
     }
 
     template<typename U>
     Sequence<Pair<T, U>>* Zip(Sequence<U>* other);
 
     Sequence<T>* EnsureArray() {
-        // Пытаемся привести текущий объект к массиву
         BaseArraySequence<T>* asArray = dynamic_cast<BaseArraySequence<T>*>(this);
         if (asArray != nullptr) {
             return asArray; 
         }
-
-        // Если не массив, создаем новый массив и копируем данные
-        // Используем MutableArraySequence напрямую, так как мы сделали forward declaration
         Sequence<T>* newArray = new MutableArraySequence<T>();
         int len = this->GetLength();
         for (int i = 0; i < len; ++i) {
@@ -111,17 +109,11 @@ public:
         return newArray;
     }
 
-    /**
-     * @brief Гарантирует, что последовательность является списком
-     */
     Sequence<T>* EnsureList() {
-        // Пытаемся привести текущий объект к списку
         BaseListSequence<T>* asList = dynamic_cast<BaseListSequence<T>*>(this);
         if (asList != nullptr) {
             return asList; 
         }
-
-        // Если не список, создаем новый список и копируем данные
         Sequence<T>* newList = new MutableListSequence<T>();
         int len = this->GetLength();
         for (int i = 0; i < len; ++i) {
@@ -130,18 +122,12 @@ public:
         return newList;
     }
 
-    /**
-     * @brief Конвертирует в массив (синоним EnsureArray для удобства)
-     */
     Sequence<T>* ToArray() {
-        return EnsureArray();
+        return EnsureArray(); 
     }
-
-    /**
-     * @brief Конвертирует в список (синоним EnsureList для удобства)
-     */
-    Sequence<T>* ToList() {
-        return EnsureList();
+    
+    Sequence<T>* ToList() { 
+        return EnsureList(); 
     }
 
     virtual ~Sequence() {}
